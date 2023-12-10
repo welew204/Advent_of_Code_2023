a057fd638be316ebb74911cce81b3bb2218333b8
Nathan Kane <kanenathan213@gmail.com>
2023-12-10 10:15:58 -0500

    day 9 work

--- nathan/test/test_day9.py
+++ nathan/test/test_day9.py
@@ -5,43 +5,17 @@ from helpers.get_file_contents import remove_line_breaks
 
 class Test_Day9(unittest.TestCase):
     data = [
-        "RL              \n",
-        "                \n",
-        "AAA = (BBB, CCC)\n",
-        "BBB = (DDD, EEE)\n",
-        "CCC = (ZZZ, GGG)\n",
-        "DDD = (DDD, DDD)\n",
-        "EEE = (EEE, EEE)\n",
-        "GGG = (GGG, GGG)\n",
-        "ZZZ = (ZZZ, ZZZ)\n",
-    ]
-    data2 = [
-        "LLR             \n",
-        "                \n",
-        "AAA = (BBB, BBB)\n",
-        "BBB = (AAA, ZZZ)\n",
-        "ZZZ = (ZZZ, ZZZ)\n",
+        "0 3 6 9 12 15    \n",
+        "1 3 6 10 15 21   \n",
+        "10 13 16 21 30 45\n",
     ]
 
     def test_day9_part1(self):
         result = day9_part1(remove_line_breaks(self.data))
-        expected_result = 2
-        expected_result2 = 6
+        expected_result = 114
         self.assertEqual(result, expected_result)
 
-    # def test_day9_part2(self):
-    #     data = [
-    #         "LR              \n",
-    #         "                \n",
-    #         "11A = (11B, XXX)\n",
-    #         "11B = (XXX, 11Z)\n",
-    #         "11Z = (11B, XXX)\n",
-    #         "22A = (22B, XXX)\n",
-    #         "22B = (22C, 22C)\n",
-    #         "22C = (22Z, 22Z)\n",
-    #         "22Z = (22B, 22B)\n",
-    #         "XXX = (XXX, XXX)\n",
-    #     ]
-    #     result = day8_part2(remove_line_breaks(data))
-    #     expected_result = 6
-    #     self.assertEqual(result, expected_result)
+    def test_day9_part2(self):
+        result = day9_part2(remove_line_breaks(self.data))
+        expected_result = 2
+        self.assertEqual(result, expected_result)
