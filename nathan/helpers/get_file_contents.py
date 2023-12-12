918ec0d925203b06a9a4e092b0ba6c786a449097
Nathan Kane <kanenathan213@gmail.com>
2023-12-12 08:36:16 -0500

    day11 (#10)

--- nathan/helpers/get_file_contents.py
+++ nathan/helpers/get_file_contents.py
@@ -4,12 +4,12 @@ from settings import ROOT_DIR
 # Join the current directory with the relative path to get the full path
 
 
-def get_file_contents(relative_path, cb, file_processor=None):
+def get_file_contents(relative_path, cb, file_processor=None, *cb_arg):
     full_path = os.path.join(ROOT_DIR, relative_path)
     with open(full_path, "r") as f:
         if file_processor:
-            return cb(file_processor(f))
-        return cb(f)
+            return cb(file_processor(f), *cb_arg)
+        return cb(f, cb_arg)
 
 
 def remove_line_breaks(file_contents):
